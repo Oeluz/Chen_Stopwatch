258a6f16cef448ab27d7815f0faa70fe7f6b9aeb
xamarin_2315 <xamarin_2315@ad.rit.edu>
2019-09-27 16:33:52 -0400

    Add project files.

---
+++ Chen_Stopwatch/ListActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Chen_Stopwatch
+{
+    [Activity(Label = "ListActivity")]
+    public class ListActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            ActionBar.Hide();
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
